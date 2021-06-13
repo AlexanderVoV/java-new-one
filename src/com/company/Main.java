a42ab4f8e193ebe2998ff2e6d39990260a37864e
alex <sashafox13@gmail.com>
2021-06-13 09:34:28 +0300

    basic commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Tenager daniil = new Tenager("Daniil", 140, 40, 10, "Male");
+        System.out.println("Name: " + daniil.getName());
+        System.out.println("Age: " + daniil.getAge() + " years");
+    }
+}
---
+++ src/com/company/Tenager.java
@@ -0,0 +1,57 @@
+package com.company;
+
+public class Tenager {
+    private String name;
+    private int height;
+    private int weight;
+    private int age;
+    private String sex;
+
+    public Tenager(String name, int height, int weight, int age, String sex) {
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
